0e7d241bbc575fa3faaf43d3608c63f17cc7ce3a
Volkmar Meineke <vmeineke@gmail.com>
2020-03-06 18:46:01 +0100

    12. commit

--- jeelink.js
+++ jeelink.js
@@ -32,16 +32,16 @@ function startAdapter(options) {
         // is called if a subscribed object changes
         objectChange: function (id, obj) {
             // Warning, obj can be null if it was deleted
-            adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
+            //adapter.log.info('objectChange ' + id + ' ' + JSON.stringify(obj));
         },
         // is called if a subscribed state changes
         stateChange: function (id, state) {
             // Warning, state can be null if it was deleted
-            adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
+        //    adapter.log.info('stateChange ' + id + ' ' + JSON.stringify(state));
         
             // you can use the ack flag to detect if it is status (true) or command (false)
-            if (state && !state.ack) {
-                adapter.log.info('ack is not set!');
+        //    if (state && !state.ack) {
+        //        adapter.log.info('ack is not set!');
             }
         },
         // is called when databases are connected and adapter received configuration.
